88a8dfaff8e30f7740ad17b1b8a9aeca3aee040e
Mike <migueljrojas@gmail.com>
2019-01-07 12:09:06 -0300

    Markup adjustments

--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -5,10 +5,46 @@ var Header = function() {
     var header = $('.header');
     var body = $('body');
     var menuOpen = $('.header__hamburguer');
+    var menuLinks = $('.header__menu li');
 
     menuOpen.on('click', function(){
         header.toggleClass('-open');
         body.toggleClass('-hideOverflow');
+    });
+
+    menuLinks.on('click', function() {
+        header.removeClass('-open');
+        body.removeClass('-hideOverflow');
+    });
+
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+        // On-page links
+        if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+        ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+            scrollTop: target.offset().top + -80
+            }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            });
+        }
+        }
     });
 };
 
